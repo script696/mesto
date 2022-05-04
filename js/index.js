73e0ae11c00ac048c610eb6991fbc2c1dcb7ae67
Nikita Semenov <script696@yandex.ru>
2022-05-04 22:11:52 +0300

    Pre finish PR5

--- js/index.js
+++ js/index.js
@@ -1,63 +1,152 @@
-/** 
- * @description Элементы DOM profile 
- */
-const editProfileButton = document.querySelector('.profile-info__edit-button');
-const profileName = document.querySelector('.profile-info__name');
-const profileAbout = document.querySelector('.profile-info__about');
+const profileInfo = document.querySelector('.profile-info');
+const editProfileButton = profileInfo.querySelector('.profile-info__edit-button');
+const addCardButton = profileInfo.querySelector('.profile-info__add-button');
+const profileName = profileInfo.querySelector('.profile-info__name');
+const profileAbout = profileInfo.querySelector('.profile-info__about');
+
+const formProfileElement = document.querySelector('.form_target_profile');
+const inputProfileName = formProfileElement.querySelector('.form__text_position_top');
+const inputProfileAbout = formProfileElement.querySelector('.form__text_position_bottom');
+
+const formCardsElement = document.querySelector('.form_target_cards');
+const inputCardsName = formCardsElement.querySelector('.form__text_position_top');
+const inputCardsLink = formCardsElement.querySelector('.form__text_position_bottom');
+
+const popupProfile = document.querySelector('.popup_target_edit-profile');
+const profileCloseButton = popupProfile.querySelector('.popup__close-button');
+const popupProfileBody = popupProfile.querySelector('.popup__body');
+
+const popupCards = document.querySelector('.popup_target_add-card');
+const cardsCloseButton = popupCards.querySelector('.popup__close-button');
+
+const popupCardFullscreen = document.querySelector('.popup_target_card-fullscreen');
+const fullscreenCloseButton = popupCardFullscreen.querySelector('.popup__close-button');
+const figureImage = popupCardFullscreen.querySelector('.figure__img');
+const figureName = popupCardFullscreen.querySelector('.figure__name');
+
+const cardsElement = document.querySelector('.cards');
+const cardTemplate = document.querySelector('#card').content;
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 
 /** 
- * @description Элементы DOM form 
+ * @description Создание нового DOM элемента (Карточка).
+ * @param {string} cardLink - Ссылка на изображение
+ * @param {string} cardName - Название изображения
+ * @returns {object} - Элемент DOM
  */
-const formElement = document.querySelector('.form');
-const inputName = formElement.querySelector('.form__text_type_name');
-const inputAbout = formElement.querySelector('.form__text_type_about');
+const createCard = (cardLink, cardName) => {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  cardElement.querySelector('.card__img').src = cardLink;
+  cardElement.querySelector('.card__img').alt = cardName;
+  cardElement.querySelector('.card__place-name').textContent = cardName;
+  return cardElement
+}
 
-/** 
- * @description Элементы DOM popup 
- */
-const popup = document.querySelector('.popup');
-const closePopupButton = popup.querySelector('.popup__close-button');
+
 
 /** 
  * @description Открытие попап, добавление текстовых значений полей элементов profileName/profileAbout  
  * в свойства value элементов inputName/inputAbout
  */
-const popupOpen = () => {
-  inputName.value = profileName.textContent;
-  inputAbout.value = profileAbout.textContent;
-  popup.classList.add('popup_opend');
+const popupProfileOpen = () => {
+  inputProfileName.value = profileName.textContent;
+  inputProfileAbout.value = profileAbout.textContent;
+  popupProfile.classList.add('popup_opend');
+  // popupProfileBody.classList.add('popup__body_opened');
 }
 
 /** 
  * @description Закрытие попап
  */
- const popupClose = () => popup.classList.remove('popup_opend');
+const popupClose = (popupName) => popupName.classList.remove('popup_opend');
 
 /** 
- * @description Запись значений полей в форму, закрытие попапа
+ * @description Запись значений полей в форму formProfileSubmit, закрытие попапа popupProfile
  * @param {object} e - Событие
 */
-const formSubmit = (e) => {
+const formProfileSubmit = (e) => {
   e.preventDefault();
-  profileName.textContent = inputName.value;
-  profileAbout.textContent = inputAbout.value;
-  popupClose()
+  profileName.textContent = inputProfileName.value;
+  profileAbout.textContent = inputProfileAbout.value;
+  popupClose(popupProfile)
 }
 
 /** 
- * @description Слушатель кнопки editProfileButton - открытие попапа 
- */
-editProfileButton.addEventListener('click', popupOpen);
+ * @description Запись значений полей в форму formCardsSubmit, закрытие попапа popupCards
+ * @param {object} e - Событие
+*/
+const formCardsSubmit = (e) => {
+  e.preventDefault();
+  const newCard = createCard(inputCardsLink.value, inputCardsName.value);
+  cardsElement.prepend(newCard);
+  popupClose(popupCards);
+}
+
+const popupFullscreenOpen = (e) => {
+    figureImage.src = e.target.src;
+    figureName.textContent = e.target.alt
+    popupCardFullscreen.classList.add('popup_opend');
+}
 
 /** 
- * @description Слушатель кнопки closePopupButton - закрытие попапа 
- */
-closePopupButton.addEventListener('click', popupClose);
-
-/**  
- * @description Слушатели формы formElement 
+ * @description Удаление элемента карточки с классом '.card' при нажатии на кнопку с классом  'card__garbage'
+ * @param {object} e - Событие
 */
-formElement.addEventListener('submit', formSubmit);
+const cardRemove = (e) => {
+    let parentElem = e.target.closest('.card')
+    parentElem.remove()
+}
+
+initialCards.forEach(elem => cardsElement.append(createCard(elem.link, elem.name)))
+
+
+editProfileButton.addEventListener('click', popupProfileOpen)
+profileCloseButton.addEventListener('click', () => popupClose(popupProfile))
+
+addCardButton.addEventListener('click', () => popupCards.classList.add('popup_opend'));
+cardsCloseButton.addEventListener('click', () => popupClose(popupCards))
+
+formProfileElement.addEventListener('submit', formProfileSubmit);
+
+formCardsElement.addEventListener('submit', formCardsSubmit);
+
+cardsElement.addEventListener('click', (e) => {
+  if (e.target.classList.contains('card__garbage')) {cardRemove(e)}
+  if (e.target.classList.contains('card__img')) {popupFullscreenOpen(e)}
+  if (e.target.classList.contains('card__logo-heart')) {
+    e.target.classList.toggle('card__logo-heart_style_filled')
+  }
+})
+
+fullscreenCloseButton.addEventListener('click', () => popupClose(popupCardFullscreen))
+
 
 
 
