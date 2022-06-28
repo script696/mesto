26b08249395efb64d24bd76646eb31d1cafebfc6
Nikita Semenov <script696@yandex.ru>
2022-06-29 00:04:36 +0300

    PR8 cod finished

---
+++ utils/constants.js
@@ -0,0 +1,10 @@
+export const editProfileButton = document.querySelector('.profile-info__edit-button');
+export const addCardButton = document.querySelector('.profile-info__add-button');
+
+export const formProfileElement = document.querySelector('.form_target_profile');
+export const formAddCardElement = document.querySelector('.form_target_cards');
+
+export const figureImage = document.querySelector('.figure__img');
+export const figureName = document.querySelector('.figure__name');
+export const cardsContainerSelector = '.cards';
+
